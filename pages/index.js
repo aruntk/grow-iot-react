250721803716dd8dd4a5862a2ac9e45e60b33127
aruntk <aruntknambiar@gmail.com>
2017-02-16 17:39:03 +0530

    basic layout with sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,51 @@
 import React from 'react'
 import Router from 'next/router'
-
+import Sidebar from 'react-sidebar'
+import MaterialTitlePanel from '../components/material_title_panel'
+import SidebarContent from '../components/sidebar_content'
 import Modal from '../components/modal'
+import Head from '../components/head'
 
 export default class extends React.Component {
   static getInitialProps () {
     return {
-      things: new Array(15).fill(0).map((v, k) => k + 1)
+      things: new Array(15).fill(0).map((v, k) => k + 1),
     }
   }
-
   constructor (props) {
     super(props)
+    this.state = {docked: true, open: false, pullRight: true}
     this.onKeyDown = this.onKeyDown.bind(this)
   }
 
   // handling escape close
   componentDidMount () {
     document.addEventListener('keydown', this.onKeyDown)
+    const mql = window.matchMedia(`(min-width: 800px)`);
+    mql.addListener(this.mediaQueryChanged);
+    this.setState({mql: mql, sidebarDocked: mql.matches});
   }
 
   componentWillUnmount () {
     document.removeEventListener('keydown', this.onKeyDown)
+    this.state.mql.removeListener(this.mediaQueryChanged);
   }
-
+  mediaQueryChanged() {
+    this.setState({sidebarDocked: this.state.mql.matches});
+  }
   onKeyDown (e) {
     if (!this.props.url.query.thingId) return
     if (e.keyCode === 27) {
       this.props.url.back()
     }
   }
+  toggleOpen(ev) {
+    this.setState({open: !this.state.open});
 
+    if (ev) {
+      ev.preventDefault();
+    }
+  }
   dismissModal () {
     Router.push('/')
   }
@@ -42,22 +57,60 @@ export default class extends React.Component {
 
   render () {
     const { url, things } = this.props
+    const sidebar = <SidebarContent />;
 
+    const contentHeader = (
+      <span>
+      {
+        !this.state.docked &&
+        <a onClick={() => this.toggleOpen()}
+        href="#"
+        className='contentHeaderMenuLink'>
+        =
+        </a>
+      }
+      <span> Grow IoT</span>
+      </span>);
+
+    const sidebarProps = {
+      styles: {
+        sidebar: {
+          zIndex: 0,
+          top: 0,
+          bottom: 0,
+          transition: 'transform .3s ease-out',
+          WebkitTransition: '-webkit-transform .3s ease-out',
+          willChange: 'transform',
+          overflowY: 'auto',
+        },
+      },
+      sidebar,
+      docked: this.state.docked,
+      pullRight: this.state.pullRight,
+      open: this.state.open,
+      onSetOpen: this.onSetOpen,
+    };
     return (
-      <div className='list'>
-        {
-          url.query.thingId &&
-            <Modal
-              id={url.query.thingId}
-              onDismiss={() => this.dismissModal()}
-            />
-        }
-        {
-          things.map((id) => (
-            <div key={id} className='thing'>
-              <a
-                className='thingLink'
-                href={`/thing?id=${id}`}
+      <div>
+      <Head>
+      </Head>
+      <Sidebar {...sidebarProps}>
+      <MaterialTitlePanel title={contentHeader}>
+
+      <div className='list sidebarContent'>
+      {
+        url.query.thingId &&
+        <Modal
+        id={url.query.thingId}
+        onDismiss={() => this.dismissModal()}
+        />
+      }
+      {
+        things.map((id) => (
+          <div key={id} className='thing'>
+          <a
+          className='thingLink'
+          href={`/thing?id=${id}`}
                 onClick={(e) => this.showThing(e, id)}
               >
                 Thing {id}
@@ -66,6 +119,14 @@ export default class extends React.Component {
           ))
         }
         <style jsx>{`
+          .contentHeaderMenuLink: {
+            textDecoration: none,
+            color: white,
+            padding: 8,
+          }
+          .sidebarContent: {
+            padding: 16px,
+          }
           .list {
             padding: 50px;
             text-align: center;
@@ -74,7 +135,6 @@ export default class extends React.Component {
           .thing {
             display: inline-block;
           }
-
           .thingLink {
             color: #333;
             verticalAlign: middle;
@@ -91,8 +151,11 @@ export default class extends React.Component {
           .thingLink:hover {
             borderColor: blue;
           }
-        `}</style>
+          `}</style>
       </div>
+</MaterialTitlePanel>
+</Sidebar>
+</div>
     )
   }
 }
